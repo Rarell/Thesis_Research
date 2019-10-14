f9e1227da9752e05461e91f39f69b059395d2277
Stuart Edris <sgedris@ou.edu>
2019-10-14 14:06:51 -0500

    Made corrections in the nc merging script and tested it.

--- Scripts/Data_Collection/merge_nc.py
+++ Scripts/Data_Collection/merge_nc.py
@@ -15,12 +15,33 @@ import numpy as np
 from netCDF4 import Dataset
 from glob import glob
 
+#%% Define the main function
+def main():
+    script = sys.argv[0]
+    parameters = sys.argv[1]
+    ForecastHour = sys.argv[2]
+    
+    year, month, day, source, VarName, TypeOfHeight =\
+        np.loadtxt(parameters, usecols = np.arange(1, 6+1), dtype = str, 
+                   delimiter = ',')
+    
+    ModelRun, VarSName = np.loadtxt(parameters, usecols = np.arange(8, 9+1), 
+                                    dtype = str, delimiter = ',')
+    
+    FH = np.loadtxt(ForecastHour, dtype = str, delimiter = ',')
+    
+    Var, VarFH, lat, lon, VarVD, mask, units = load_multiple_nc(VarSName, FH)
+    
+    write_nc(VarSName, VarName, Var, VarFH, lat, lon, VarVD, mask, units,
+             year, month, day, ModelRun, TypeOfHeight)
+    
+
 #%% Define a function to load in netcdf files.
-def load_nc(VarSName, VarFH):
-    path = '/Users/Rarrell/Desktop/Thesis_Research/tmp/'
-    filename = 'GFS_' + VarSName + '_' +\
-                   str(VarFH) + '.nc'
-    with Dataset(path + filename, 'r') as nc:
+def load_nc(VarSName, file, VarFH):
+#    path = '/Users/Rarrell/Desktop/Thesis_Research/tmp/'
+#    filename = 'GFS_' + str(VarSName) + '_' +\
+#                  str(VarFH) + '.nc'
+    with Dataset(file, 'r') as nc:
         if str(VarFH) == '003':
             lat = nc.variables['lat'][:]
             lon = nc.variables['lon'][:]
@@ -34,9 +55,9 @@ def load_nc(VarSName, VarFH):
         else:
             pass
         
-    FH = nc.variables['FH'][:]
+        FH = nc.variables['FH'][:]
     
-    Var = nc.variables[VarSName][:,:]
+        Var = nc.variables[str(VarSName)][:,:]
     
     if str(VarFH) == '003':
         return Var, FH, lat, lon, VD, mask, units
@@ -45,15 +66,15 @@ def load_nc(VarSName, VarFH):
     
 
 #%% Define a function to load the .nc files.
-def load_multiple_nc(VarSName, FH, SomeVar):
+def load_multiple_nc(VarSName, FH):
 
     path = '/Users/Rarrell/Desktop/Thesis_Research/tmp/'
     # Load in a temporary file.
-    with Dataset(path + 'GFS_' + VarSName + '_6.nc', 'r') as nc:
-        tmp = nc.variables[VarSName][:,:]
+    with Dataset(path + 'GFS_' + str(VarSName) + '_6.nc', 'r') as nc:
+        tmp = nc.variables[str(VarSName)][:,:]
     
     # Initialize some values.
-    T = len(glob(path + 'GFS_' + VarSName + '_*.nc'))
+    T = len(glob(path + 'GFS_' + str(VarSName) + '_*.nc'))
     J, I = tmp.shape
     
     VarFH = np.ones((T, )) * np.nan
@@ -63,37 +84,38 @@ def load_multiple_nc(VarSName, FH, SomeVar):
     mask = np.ones((J, I)) * np.nan
     
     # Load in all data points
-    for n, t in enumerate(FH):
-        if t == '003':
-            Var[:,:,n], VarFH[n], lat[:,:], lon[:,:], VarVD, mask, units =\
-            load_nc(VarSName, t)
+    for n, file in enumerate(glob(path + 'GFS_' + str(VarSName) + '_*.nc')):
+        if FH[n] == '003':
+            Var[:,:,n], VarFH[n], lat[:], lon[:], VarVD, mask, units =\
+            load_nc(VarSName, file, FH[n])
         else:
-            Var[:,:,n], VarFH[n] = load_nc(VarSName, t)
+            Var[:,:,n], VarFH[n] = load_nc(VarSName, file, FH[n])
     
     return Var, VarFH, lat, lon, VarVD, mask, units
 
 #%% Define a function to write the combined data.
 def write_nc(VarSName, VarName, Var, VarFH, lat, lon, VarVD, mask, units,
-             year, month, day, TypeOfHeight):
-    filename = 'GFS_' + VarSName + '_' + year + month + day + '.nc'
+             year, month, day, ModelRun, TypeOfHeight):
+    filename = 'GFS_' + str(VarSName) + '_' + str(year) + str(month) +\
+               str(day) + '_' + str(ModelRun) + '.nc'
     path = '/Users/Rarrell/Desktop/Thesis_Research/Data/'
     
     J, I , T = Var.shape
     
     with Dataset(path + filename, 'w', format = 'NETCDF4') as nc:
-        nc.description('GFS forecast data for ' + VarName + ' valid for ' +\
-                       day + '-' + month + '-' + year + '. The variable is' +\
-                       'given for all forecast hours.\n' +\
-                       'Variable: ' + VarName +'(' + units + '). This variable' +\
-                       ' is in the format of lat x lon x time/forecast hour.\n' +\
-                       'mask: Land - sea mask. sea = 0, land = 1.' +\
-                       'FH: Forecast hour (hour). The hour at which the ' +\
-                       'forecast is made. E.g., FH = 3 is the 3 hour forecast' +\
-                       'from ' + day + '-' + month + '-' + year + '.\n' +\
-                       'VD: Date for which the GFS model run is valid.' +\
-                       'units: The units of the variable.\n' +\
-                       'lat: Latitude of the grid (vector form).\n' +\
-                       'lon: Longitude of the grid (vector form).')
+        nc.description = 'GFS forecast data for ' + VarName + ' valid for ' +\
+                         day + '-' + month + '-' + year + '. The variable is ' +\
+                         'given for all forecast hours.\n' +\
+                         'Variable: ' + VarName +'(' + units + '). This variable ' +\
+                         'is in the format of lat x lon x time/forecast hour.\n' +\
+                         'mask: Land - sea mask. sea = 0, land = 1.\n' +\
+                         'FH: Forecast hour (hour). The hour at which the ' +\
+                         'forecast is made. E.g., FH = 3 is the 3 hour forecast ' +\
+                         'from ' + day + '-' + month + '-' + year + '.\n' +\
+                         'VD: Date for which the GFS model run is valid.\n' +\
+                         'units: The units of the variable.\n' +\
+                         'lat: Latitude of the grid (vector form).\n' +\
+                         'lon: Longitude of the grid (vector form).'
         
         # Create the spatial and temporal dimensions
         nc.createDimension('lat', size = J)
@@ -113,13 +135,13 @@ def write_nc(VarSName, VarName, Var, VarFH, lat, lon, VarVD, mask, units,
         
         # Create a variable for the forecast hour.
         nc.createVariable('FH', int, ('time', ))
-        nc.variables['FH'][:] = int(VarFH[:])
+        nc.variables['FH'][:] = VarFH[:]
         
         # Create the variable for the main variable
-        nc.createVariable(VarSName, Var.dtype, ('lat', 'lon', 'time'))
+        nc.createVariable(str(VarSName), Var.dtype, ('lat', 'lon', 'time'))
         nc.setncatts({'long_name' : VarName, 'units' : units, 
                       'level_desc' : TypeOfHeight})
-        nc.variables[VarSName][:,:,:] = Var[:,:,:]
+        nc.variables[str(VarSName)][:,:,:] = Var[:,:,:]
         
         # Create the units variable for the variable
         nc.createVariable('units', np.str)
@@ -129,4 +151,7 @@ def write_nc(VarSName, VarName, Var, VarFH, lat, lon, VarVD, mask, units,
         nc.createVariable('mask', mask.dtype, ('lat', 'lon'))
         nc.variables['mask'][:,:] = mask[:,:]
         
-    
+#%% Call the main functions
+        
+if __name__ == '__main__':
+    main()
